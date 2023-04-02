042f1e2d9dd84a48624baca6761030a527c8ddac
William Babin <will.babin53@gmail.com>
2023-04-02 18:45:12 +0200

    labyrinthe avec bord délimiter

--- lib/main.dart
+++ lib/main.dart
@@ -68,7 +68,8 @@ class _MyHomePageState extends State<MyHomePage> {
               onPressed: () {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => LabyrinthePage()),
+                  MaterialPageRoute(
+                      builder: (context) => const LabyrinthePage()),
                 );
               },
             ),
--- lib/tuto/labyrinthe.dart
+++ lib/tuto/labyrinthe.dart
@@ -3,7 +3,7 @@ import 'package:flutter/services.dart';
 import 'package:sensors_plus/sensors_plus.dart';
 
 class LabyrinthePage extends StatefulWidget {
-  const LabyrinthePage({super.key});
+  const LabyrinthePage({Key? key});
 
   @override
   _LabyrintheState createState() => _LabyrintheState();
@@ -12,21 +12,25 @@ class LabyrinthePage extends StatefulWidget {
 class _LabyrintheState extends State<LabyrinthePage> {
   double _ballPositionX = 0.0;
   double _ballPositionY = 0.0;
+  double sizeBall = 30.0;
+  double speed = 50.0;
 
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
+    double appBarHeight =
+        AppBar().preferredSize.height + MediaQuery.of(context).padding.top;
 
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        backgroundColor: Theme.of(context).colorScheme.secondary,
         title: const Text("Labyrinthe"),
       ),
       body: Stack(
         children: [
           AnimatedPositioned(
-            duration: Duration(milliseconds: 200),
+            duration: const Duration(milliseconds: 200),
             curve: Curves.easeInOut,
             left: _ballPositionX,
             bottom: _ballPositionY,
@@ -36,7 +40,7 @@ class _LabyrintheState extends State<LabyrinthePage> {
               child: Icon(
                 Icons.sports_volleyball,
                 color: Colors.red,
-                size: 50,
+                size: sizeBall,
               ),
             ),
           ),
@@ -46,10 +50,15 @@ class _LabyrintheState extends State<LabyrinthePage> {
             child: ElevatedButton(
               onPressed: () {
                 setState(() {
-                  _ballPositionY -= 30;
+                  if (_ballPositionY - speed > sizeBall / 4) {
+                    _ballPositionY -= speed;
+                  } else {
+                    _ballPositionY = 0 - sizeBall / 4;
+                  }
+                  print(_ballPositionY);
                 });
               },
-              child: Text('Bas'),
+              child: const Text('Bas'),
             ),
           ),
           Positioned(
@@ -58,10 +67,17 @@ class _LabyrintheState extends State<LabyrinthePage> {
             child: ElevatedButton(
               onPressed: () {
                 setState(() {
-                  _ballPositionY += 30;
+                  if (_ballPositionY + speed <
+                      screenHeight - appBarHeight - sizeBall - 20) {
+                    _ballPositionY += speed;
+                  } else {
+                    _ballPositionY =
+                        screenHeight - appBarHeight - sizeBall - 10;
+                  }
+                  print(_ballPositionY);
                 });
               },
-              child: Text('Haut'),
+              child: const Text('Haut'),
             ),
           ),
           Positioned(
@@ -70,12 +86,15 @@ class _LabyrintheState extends State<LabyrinthePage> {
             child: ElevatedButton(
               onPressed: () {
                 setState(() {
-                  if (_ballPositionX > 0) {
-                    _ballPositionX -= 30;
+                  if (_ballPositionX - speed > 0) {
+                    _ballPositionX -= speed;
+                  } else {
+                    _ballPositionX = 0 - sizeBall / 4;
                   }
+                  print(_ballPositionX);
                 });
               },
-              child: Text('Gauche'),
+              child: const Text('Gauche'),
             ),
           ),
           Positioned(
@@ -84,12 +103,15 @@ class _LabyrintheState extends State<LabyrinthePage> {
             child: ElevatedButton(
               onPressed: () {
                 setState(() {
-                  if (_ballPositionX < screenWidth - 60) {
-                    _ballPositionX += 30;
+                  if (_ballPositionX + speed < screenWidth - sizeBall - 20) {
+                    _ballPositionX += speed;
+                  } else {
+                    _ballPositionX = screenWidth - sizeBall - 10;
                   }
+                  print(_ballPositionX);
                 });
               },
-              child: Text('Droite'),
+              child: const Text('Droite'),
             ),
           ),
         ],
